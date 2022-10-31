6d276554f1c138d2175cb342d9af5b2f900e4eea
Michael Warwick <michael.warwick@warrenandmahoney.com>
2022-10-31 16:13:13 +1300

    Added pyRevt extension
    A button to open Notepad++ and a button to open N++ with the formulas from the current family ready to edit.

---
+++ pyRevit/pyMAW.tab/Notepad++.panel/Notepad++.pushbutton/script.py
@@ -0,0 +1,6 @@
+from pyrevit import forms
+from pyrevit import revit, DB
+import os
+import subprocess
+
+subprocess.Popen([],executable='C:\\Users\\Warwickm\\OneDrive - Warren and Mahoney\\Documents\\PortableApps\\Notepad++Portable\\Notepad++Portable.exe')
