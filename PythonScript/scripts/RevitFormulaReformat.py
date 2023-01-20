16c856c33da65a1f0594eb8c3147cd7d4578a8f9
Michael Warwick <mwarwick@studioda.co.nz>
2023-01-20 17:47:53 +1300

    Added script to reformat Revit formula to make readable in Notepad++

---
+++ PythonScript/scripts/RevitFormulaReformat.py
@@ -0,0 +1,78 @@
+import re
+
+original_string = ""
+new_string = ""
+indent_level = 0
+extra_len = 0
+indent = "\t"
+text = []
+isSelection = False
+
+# editor.newLine()
+# editor.insertText(curPos, fLine)
+
+# if(test statement, if(and(one, two, three), "one true", if(another conditional test, "all true, really", "alternative")), "otherwise")
+
+# input
+lineNo = editor.lineFromPosition(editor.getCurrentPos())
+isSelection = not editor.getSelectionEmpty()
+if isSelection:
+    original_string = editor.getSelText()
+else:
+    editor.home()
+    original_string = editor.getLine(lineNo)
+# original_string = "if(test statement, if(and(one, two, three), \"one true\", if(another conditional test, \"all true, really\", \"alternative\")), otherwise)"
+
+# save strings out
+def text_sub(match):
+    global text
+    text.append(match[0])
+    return "{t[" + str(len(text)-1) + "]}"
+new_string = re.sub(r"\".+?\"", text_sub, original_string)
+
+# add returns
+new_string = re.sub(r",", ",\n", new_string)
+new_string = re.sub(r"\(", "(\n", new_string)
+new_string = re.sub(r"\)", "\n)\n", new_string)
+new_string = re.sub(r"\n\s+", "\n", new_string)
+new_string = re.sub(r"\)\n,", "),", new_string)
+
+# indent
+pos = 0
+extra_len = 0
+next_indent_inc = 0
+for st in re.finditer('\n', new_string + "\n"):
+    # parse indent level
+    # ')' and ',' apply to current line
+    # '(' applies from next line on
+    match new_string[st.start() - 1 + extra_len]:
+        case "(":
+            indent_level += next_indent_inc + 0
+            next_indent_inc = 1
+        case ")":
+            indent_level += next_indent_inc - 1
+            next_indent_inc = 0
+        case ",":
+            if new_string[st.start() - 2 + extra_len] == ")":
+                indent_level -= 1
+            indent_level += next_indent_inc
+            next_indent_inc = 0
+        case _:
+            indent_level += next_indent_inc
+            next_indent_inc = 0
+    # apply indent to start of current line
+    new_string = new_string[:pos] + indent*indent_level + new_string[pos:]
+    extra_len +=  len(indent) * indent_level
+    pos = st.end() + extra_len
+
+# restore strings
+new_string = new_string.format(t=text)
+
+# output
+editor.beginUndoAction()
+# console.write(new_string + "\r\n===\r\n")
+if isSelection:
+    editor.replaceSel(new_string)
+else:
+    editor.replaceWholeLine(lineNo, new_string)
+editor.endUndoAction()
