364ed6310477f852dcf531c810df246ecd809da3
Michael Warwick <michael.warwick@warrenandmahoney.com>
2023-02-28 17:33:25 +1300

    Added pyMAW buttons for opening additional support files

---
+++ pyRevit/pyMAW.extension/pyMAW.tab/Notepad++.panel/OpenNotepadpp.splitbutton/IFCCategories.pushbutton/script.py
@@ -0,0 +1,3 @@
+import Notepadpp as npp
+
+npp.Open(file="ifc export categories")
--- pyRevit/pyMAW.extension/pyMAW.tab/Notepad++.panel/lib/Notepadpp.py
+++ pyRevit/pyMAW.extension/pyMAW.tab/Notepad++.panel/lib/Notepadpp.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 from pyrevit import revit
 from pyrevit import script
+from pyrevit import HOST_APP
 import os
 import subprocess
 
 def Open (file = "", path = "", syntax = "", exepath = None):
+    revit_ver = HOST_APP.version
+    logger = script.get_logger()
+    
     # get Notepad path
     if exepath == None:
         cfg = script.get_config("Notepad++")
-        exepath = cfg.get_option('notepadpath', os.path.join(os.environ["ProgramFiles"],"Notepad++", "Notepad++.exe"))
+        exepath = cfg.get_option('notepadpath', os.path.join(os.environ["ProgramFiles"], "Notepad++", "Notepad++.exe"))
 
     # get file path
     if file == "keynote":
@@ -20,23 +24,42 @@ def Open (file = "", path = "", syntax = "", exepath = None):
         app = __revit__.Application
         path = app.SharedParametersFilename
         syntax = "-lprops"
+    elif file == "user revit.ini":
+        # get user revit.ini file path
+        path = os.path.expandvars("%APPDATA%\Autodesk\Revit\Autodesk Revit {0}\Revit.ini".format(revit_ver))
+        syntax = "-lini"
+    elif file == "default revit.ini":
+        # get default revit.ini file path
+        path = os.path.expandvars("%ALLUSERSPROFILE%\Autodesk\RVT {0}\UserDataCache\Revit.ini".format(revit_ver))
+        syntax = "-lini"
     elif file == "keyboard shortcuts":
         # get keyboard shortcuts file path
-        path = os.path.expandvars("%APPDATA%\Autodesk\Revit\Autodesk Revit 2021\KeyboardShortcuts.xml")
+        path = os.path.expandvars("%APPDATA%\Autodesk\Revit\Autodesk Revit {0}\KeyboardShortcuts.xml".format(revit_ver))
         syntax = "-lxml"
+    elif file == "revit server settings":
+        # get revit server settings file path
+        path = os.path.expandvars("%PROGRAMDATA%\Autodesk\Revit Server {0}\Config\RSN.ini".format(revit_ver))
+        syntax = "-lini"
+    elif file == "ifc export categories":
+        # get IFC export category table file path
+        app = __revit__.Application
+        path = app.ExportIFCCategoryTable
+        syntax = "-lpowershell"
     elif file == "current journal":
         # get journal file file path
         path = revit.get_current_journal_file()
         syntax = "-lvb -n9999999999"
-# - Revit ini (user/install)
-# - C:\Users\mwarwick\AppData\Roaming\Autodesk\Revit\Autodesk Revit 2021\RevitUILayout.xml
-# - environment file
+    # other ideas:
+    # - C:\Users\mwarwick\AppData\Roaming\Autodesk\Revit\Autodesk Revit 2021\RevitUILayout.xml
+    # - environment file
     else:
         path = ""
         syntax = ""
     
     # open Notepad(++) with file
-    path = os.path.realpath(path)
     if not exepath is None and os.path.exists(exepath):
-        command = u'start "Notepad++" {0} {2} "{1}"'.format(exepath, path, syntax)
+        if len(path) > 0:
+            path = '"' + os.path.realpath(path) + '"'
+        command = u'start "Notepad++" "{0}" {1} {2}'.format(exepath, syntax, path)
+        logger.debug(command)
         os.system(command)
